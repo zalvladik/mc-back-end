63177996269eb00fb85cc27989b3a3a45c97dba9
Alex Tkachenko <alex.t@2muchcoffee.com>
2024-07-03 01:32:07 +0300

    added countsShulker and lot to getUser

--- src/modules-site/user/services/user.service.ts
+++ src/modules-site/user/services/user.service.ts
@@ -16,7 +16,7 @@ export class UserService {
   async getByID(id: number): Promise<GetProfileResponseDto> {
     return this.userRepository.findOne({
       where: { id },
-      select: ['id', 'username', 'role', 'money'],
+      select: ['id', 'username', 'role', 'money', 'countShulker', 'countLot'],
     })
   }
 }

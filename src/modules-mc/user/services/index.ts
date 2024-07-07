424bf15c7ac257188e836013e39944c15d67eb9a
VladislavZ <zalipkav@ukr.net>
2024-07-07 12:35:17 +0300

    master

--- src/modules-mc/user/services/index.ts
+++ src/modules-mc/user/services/index.ts
@@ -1,7 +1,7 @@
 import { UserAdvancementsService } from './user-advancements.service'
 import { UserItemsService } from './user-items.service'
 import { UserMoneyService } from './user-money.service'
-import { UserShulkersService } from './user.shulkers.service'
+import { UserShulkersService } from './user-shulkers.service'
 
 export {
   UserAdvancementsService,

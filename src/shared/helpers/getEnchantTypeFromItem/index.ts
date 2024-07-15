3815ee8157d56f342f6db24d26bbf412025e0284
Alex Tkachenko <alex.t@2muchcoffee.com>
2024-07-15 11:25:14 +0300

    changed getEnchantTypeFromItemType

--- src/shared/helpers/getEnchantTypeFromItem/index.ts
+++ src/shared/helpers/getEnchantTypeFromItem/index.ts
@@ -3,39 +3,39 @@ import { EnchantsTypesEnum } from 'src/shared/enums'
 export const getEnchantTypeFromItemType = (
   itemType: string,
 ): EnchantsTypesEnum => {
-  if (itemType.includes(EnchantsTypesEnum.HELMET)) {
+  if (itemType.includes(`_${EnchantsTypesEnum.HELMET}`)) {
     return EnchantsTypesEnum.HELMET
   }
 
-  if (itemType.includes(EnchantsTypesEnum.CHESTPLATE)) {
+  if (itemType.includes(`_${EnchantsTypesEnum.CHESTPLATE}`)) {
     return EnchantsTypesEnum.CHESTPLATE
   }
 
-  if (itemType.includes(EnchantsTypesEnum.LEGGINGS)) {
+  if (itemType.includes(`_${EnchantsTypesEnum.LEGGINGS}`)) {
     return EnchantsTypesEnum.LEGGINGS
   }
 
-  if (itemType.includes(EnchantsTypesEnum.BOOTS)) {
+  if (itemType.includes(`_${EnchantsTypesEnum.BOOTS}`)) {
     return EnchantsTypesEnum.BOOTS
   }
 
-  if (itemType.includes(EnchantsTypesEnum.SWORD)) {
+  if (itemType.includes(`_${EnchantsTypesEnum.SWORD}`)) {
     return EnchantsTypesEnum.SWORD
   }
 
-  if (itemType.includes(EnchantsTypesEnum.PICKAXE)) {
+  if (itemType.includes(`_${EnchantsTypesEnum.PICKAXE}`)) {
     return EnchantsTypesEnum.PICKAXE
   }
 
-  if (itemType.includes(EnchantsTypesEnum.AXE)) {
+  if (itemType.includes(`_${EnchantsTypesEnum.AXE}`)) {
     return EnchantsTypesEnum.AXE
   }
 
-  if (itemType.includes(EnchantsTypesEnum.SHOVEL)) {
+  if (itemType.includes(`_${EnchantsTypesEnum.SHOVEL}`)) {
     return EnchantsTypesEnum.SHOVEL
   }
 
-  if (itemType.includes(EnchantsTypesEnum.HOE)) {
+  if (itemType.includes(`_${EnchantsTypesEnum.HOE}`)) {
     return EnchantsTypesEnum.HOE
   }
 

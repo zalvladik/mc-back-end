92326771df90100db8e4a5440b92b5b152e0db0a
Alex Tkachenko <alex.t@2muchcoffee.com>
2024-07-21 13:32:59 +0300

    changed logic with whitelist

--- src/modules-site/whitelist/controllers/whitelist.controller.ts
+++ src/modules-site/whitelist/controllers/whitelist.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common'
+import { Body, Controller, Get, Logger, Post, Query } from '@nestjs/common'
 import { WhitelistService } from '../services'
 import { CheckIsExistUserQueryDto } from '../dtos-request'
 
 @Controller('whitelist')
 export class WhitelistController {
+  private logget = new Logger('WhitelistController')
+
   constructor(private readonly whitelistService: WhitelistService) {}
 
   @Get()
@@ -15,6 +17,7 @@ export class WhitelistController {
 
   @Post()
   async addUser(@Body() body: any): Promise<void> {
+    this.logget.verbose(body)
     await this.whitelistService.addUser(body)
   }
 }

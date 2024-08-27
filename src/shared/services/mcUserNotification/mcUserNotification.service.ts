94798537e2ae7a8881c7d0e952df001b162835c0
Alex Tkachenko <alex.t@2muchcoffee.com>
2024-08-27 21:06:19 +0300

    fixed McUserNotificationService

--- src/shared/services/mcUserNotification/mcUserNotification.service.ts
+++ src/shared/services/mcUserNotification/mcUserNotification.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 import axios from 'axios'
 import type {
   ByeItemLotNotificationT,
@@ -7,16 +7,24 @@ import type {
 
 @Injectable()
 export class McUserNotificationService {
+  private logger = new Logger('McUserNotificationService')
+
+  private minecraftServerURL = process.env.MINECRAFT_SERVER_URL
+
   async byeItemLotNotification({
     username,
     message,
     serialized,
   }: ByeItemLotNotificationT): Promise<void> {
-    await axios.post('http://51.75.74.159:25579/byeItemLotNotification', {
-      username,
-      message,
-      serialized,
-    })
+    try {
+      await axios.post(`${this.minecraftServerURL}/byeItemLotNotification`, {
+        username,
+        message,
+        serialized,
+      })
+    } catch (error) {
+      this.logger.verbose(error)
+    }
   }
 
   async byeShulkerLotNotification({
@@ -24,10 +32,14 @@ export class McUserNotificationService {
     message,
     serializedArray,
   }: ByeShulkerLotNotificationT): Promise<void> {
-    await axios.post('http://51.75.74.159:25579/byeShulkerLotNotification', {
-      username,
-      message,
-      serializedArray,
-    })
+    try {
+      await axios.post(`${this.minecraftServerURL}/byeShulkerLotNotification`, {
+        username,
+        message,
+        serializedArray,
+      })
+    } catch (error) {
+      this.logger.verbose(error)
+    }
   }
 }

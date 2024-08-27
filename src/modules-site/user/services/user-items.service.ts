afaf96e8ed0956bd35ff4b93003aafd5fbd862b8
Alex Tkachenko <alex.t@2muchcoffee.com>
2024-08-27 23:55:19 +0300

    fix getItemsFromUser

--- src/modules-site/user/services/user-items.service.ts
+++ src/modules-site/user/services/user-items.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 
-import { Brackets, Repository } from 'typeorm'
+import { IsNull, Repository } from 'typeorm'
 
 import { Item } from 'src/entities/item.entity'
+import { itemMeta } from 'src/shared/constants'
 import type { GetItemsFromUserResponseDto } from '../dtos-response'
 
 @Injectable()
@@ -14,28 +15,28 @@ export class UserItemsService {
   ) {}
 
   async getItemsFromUser(id: number): Promise<GetItemsFromUserResponseDto[]> {
-    return this.itemRepository
-      .createQueryBuilder('item')
-      .leftJoinAndSelect('item.lot', 'lot') // Присоединяем таблицу `lot` для использования в условиях
-      .where('item.userId = :id', { id })
-      .andWhere('item.itemTicketId IS NULL')
-      .andWhere('item.isTaken = false')
-      .andWhere('item.shulkerId IS NULL')
-      .andWhere(
-        new Brackets(qb => {
-          qb.where('item.lotId IS NULL').orWhere('lot.isSold = true')
-        }),
-      )
-      .select([
-        'id',
-        'amount',
-        'categories',
-        'description',
-        'display_name',
-        'durability',
-        'enchants',
-        'type',
-      ])
-      .getMany()
+    const itemsWithoutLot = await this.itemRepository.find({
+      where: {
+        user: { id },
+        itemTicket: IsNull(),
+        isTaken: false,
+        lot: IsNull(),
+        shulker: IsNull(),
+      },
+      select: itemMeta,
+    })
+
+    const itemsWithBuyetLot = await this.itemRepository.find({
+      where: {
+        user: { id },
+        itemTicket: IsNull(),
+        isTaken: false,
+        lot: { isSold: true },
+        shulker: IsNull(),
+      },
+      select: itemMeta,
+    })
+
+    return [...itemsWithoutLot, ...itemsWithBuyetLot]
   }
 }

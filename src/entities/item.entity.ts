4d9efd35a277779be157ba5d65b79b338f35d975
Alex Tkachenko <alex.t@2muchcoffee.com>
2024-08-28 12:03:05 +0300

    fixed delete lot and item_tocken

--- src/entities/item.entity.ts
+++ src/entities/item.entity.ts
@@ -73,7 +73,7 @@ export class Item {
   @JoinColumn({ name: 'item_ticket_id' })
   itemTicket: ItemTicket
 
-  @OneToOne(() => Lot, lot => lot.item)
+  @OneToOne(() => Lot, lot => lot.item, { onDelete: 'SET NULL' })
   @JoinColumn({ name: 'lot_id' })
   lot: Lot
 

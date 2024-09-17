b96b6518267c486a504cb06b3c6ef84a29ca0dee
Alex Tkachenko <alex.t@2muchcoffee.com>
2024-09-17 11:57:15 +0300

    fixed formatDateToSQL

--- src/shared/helpers/formatDateToSQL/index.ts
+++ src/shared/helpers/formatDateToSQL/index.ts
@@ -1,6 +1,12 @@
-export const formatDateToSQL = (date: Date): string => {
+export const formatDateToSQL = (dateInput: any): string => {
+  const date = dateInput instanceof Date ? dateInput : new Date(dateInput)
+
+  if (Number.isNaN(date.getTime())) {
+    throw new Error('Invalid date')
+  }
+
   const year = date.getFullYear()
-  const month = String(date.getMonth() + 1).padStart(2, '0') // Месяцы начинаются с 0
+  const month = String(date.getMonth() + 1).padStart(2, '0')
   const day = String(date.getDate()).padStart(2, '0')
   const hours = String(date.getHours()).padStart(2, '0')
   const minutes = String(date.getMinutes()).padStart(2, '0')
